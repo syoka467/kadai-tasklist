2f59442b7d43022a67badd8a15c5e068bcc7a60c
kazuki.yanagisawa <syoka467@gmail.com>
2020-09-05 09:57:23 +0000

    five commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,8 +44,8 @@ class TasksController < ApplicationController
         @task = Task.find(params[:id])
         @task.destroy
 
-        flash[:success] = 'Message は正常に削除されました'
-        redirect_to messages_url
+        flash[:success] = 'Task は正常に削除されました'
+        redirect_to tasks_url
     end
     
     
